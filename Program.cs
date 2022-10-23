909e51c084830b5087016f8377d5075ca317b6ab
Bradley <94942363+Larsa-mrp@users.noreply.github.com>
2022-10-23 18:07:08 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using static System.Console;
+
+namespace Program
+{
+    class CustomLinkedList
+    {
+        Node head;
+
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int d) { data = d; }
+        }
+        public void deleteBackHalf()
+        {
+            if (head == null || head.next == null)
+            {
+                head = null;
+            }
+
+            Node slow = head;
+            Node fast = head;
+            Node previous = null;
+
+            while (fast != null && fast.next != null)
+            {
+                previous = slow;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            previous.next = null;
+        }
+        public void displayContents()
+        {
+            Node current = head;
+
+            while (current != null)
+            {
+                Console.Write(current.data + "->");
+                current = current.next;
+            }
+        }
+
+        public void deleteNthNodeFromEnd(int n)
+        {
+            if (head == null || n == 0)
+            {
+                return;
+            }
+
+            Node first = head;
+            Node second = head;
+
+            for (int i = 0; i < n; i++)
+            {
+                second = second.next;
+                if (second.next == null)
+                {
+                    // N >= length of list
+                    if (i == n - 1)
+                    {
+                        head = head.next;
+                    }
+                    return;
+                }
+            }
+            while (second.next != null)
+            {
+                first = first.next;
+                second = second.next;
+            }
+            first.next = first.next.next;
+        }
+
+        static void Main(string[] args)
+        {
+            CustomLinkedList linkedList = new CustomLinkedList();
+            Node firstNode = new Node(45);
+            Node secondNode = new Node(35);
+            Node thirdNode = new Node(18);
+            Node fourthNode = new Node(9);
+            Node fifthNode = new Node(19);
+            Node sixthNode = new Node(39);
+            Node seventhNode = new Node(49);
+            Node eighthNode = new Node(23);
+
+            linkedList.head = firstNode;
+            firstNode.next = secondNode;
+            secondNode.next = thirdNode;
+            thirdNode.next = fourthNode;
+            fourthNode.next = fifthNode;
+            fifthNode.next = sixthNode;
+            sixthNode.next = seventhNode;
+            seventhNode.next = eighthNode;
+
+            linkedList.displayContents();
+            WriteLine();
+            linkedList.deleteNthNodeFromEnd(2);
+            WriteLine();
+            linkedList.displayContents();
+            WriteLine();
+            linkedList.deleteBackHalf();
+            WriteLine();
+            linkedList.displayContents();
+
+        }
+    }
+}
